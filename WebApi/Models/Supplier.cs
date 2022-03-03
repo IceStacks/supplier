8881ece060c68db798940f329949d458cb59d3cd
Ahmet Osman Sezgin <ahmet.osmn@outlook.com>
2022-03-03 22:46:38 +0300

    Supplier yeni prop lar eklendi

--- WebApi/Models/Supplier.cs
+++ WebApi/Models/Supplier.cs
@@ -3,12 +3,14 @@ namespace WebApi.Models
     public class Supplier
     {
         public int Id { get; set; }
-        public int ProductId { get; set; }
-        public int SellerId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
         public string Address { get; set; }
-        public string Address2 { get; set; }
-        public string City { get; set; }
-        public string PhoneNumber { get; set; }
         public string Mail { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyMail { get; set; }
+        public string CompanyPhoneNumber { get; set; }
     }
 }
