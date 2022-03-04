f5f42022ad67c42399b080d5b24b6b215462f667
Ahmet Osman Sezgin <ahmet.osmn@outlook.com>
2022-03-04 14:29:49 +0300

    Show ve Store endpoint'leri yamamlandı

--- WebApi/Controllers/SupplierController.cs
+++ WebApi/Controllers/SupplierController.cs
@@ -21,56 +21,98 @@ namespace WebApi.Controllers
         //     _context = context;
         // }
 
+        // asagida yazdigim db baglantilari duzeltilecek. 
+        // DI ile yapmaya calisacagim,  simdilik gecici olarak bu sekilde kullandim.
+
+        MySqlConnection connection = new MySqlConnection("Server=localhost;Database=IceStacks-Supplier;Uid=root;Pwd=55255Ahmet_;");
+
         [HttpGet]
         public IActionResult Index()
         {
-            List<Supplier> suppliers = new List<Supplier>();
+            List<GetSuppliersViewModel> suppliers = new List<GetSuppliersViewModel>();
 
-            using(MySqlConnection connection = new MySqlConnection("Server=localhost;Database=IceStacks-Supplier;Uid=root;Pwd=55255Ahmet_"))
+            connection.Open();
+
+            MySqlCommand cmd = new MySqlCommand("select * from Suppliers", connection);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while(reader.Read())
             {
-                connection.Open();
-                MySqlCommand cmd = new MySqlCommand("select * from Suppliers", connection);
-                MySqlDataReader reader = cmd.ExecuteReader();
+                GetSuppliersViewModel supplier = new GetSuppliersViewModel();
 
-                while(reader.Read())
-                {
-                    Supplier supplier = new Supplier();
-                    supplier.Id = Convert.ToInt32(reader["id"]);
-                    supplier.Name = reader["name"].ToString();
-                    supplier.Surname = reader["surname"].ToString();
-                    supplier.Gender = reader["gender"].ToString();
-                    supplier.Address = reader["address"].ToString();
-                    supplier.Mail = reader["mail"].ToString();
-                    supplier.Phone = reader["phone"].ToString();
-                    supplier.CompanyName = reader["company_name"].ToString();
-                    supplier.CompanyMail = reader["company_mail"].ToString();
-                    supplier.CompanyPhone = reader["company_phone"].ToString();
+                supplier.Name = reader["name"].ToString();
+                supplier.Surname = reader["surname"].ToString();
+                supplier.Gender = reader["gender"].ToString();
+                supplier.Address = reader["address"].ToString();
+                supplier.Mail = reader["mail"].ToString();
+                supplier.Phone = reader["phone"].ToString();
+                supplier.CompanyName = reader["company_name"].ToString();
+                supplier.CompanyMail = reader["company_mail"].ToString();
+                supplier.CompanyPhone = reader["company_phone"].ToString();
 
-                    suppliers.Add(supplier);
-                }
-
-                reader.Close();
-                connection.Close();
+                suppliers.Add(supplier);
             }
+
+            reader.Close();
+            connection.Close();
             return Ok(suppliers);
         }
 
         [HttpGet("{id}")]
-        public IActionResult Show(int id)
+        public IActionResult Show(int id) // model ile olmali
         {
-            return Ok();
+            connection.Open();
+
+            MySqlCommand cmd = new MySqlCommand("select * from Suppliers where id = @id", connection);
+            cmd.Parameters.AddWithValue("@id", id);
+            
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            GetSupplierDetailViewModel supplier = new GetSupplierDetailViewModel();
+
+            while(reader.Read())
+            {
+                supplier.Name = reader["name"].ToString();
+                supplier.Surname = reader["surname"].ToString();
+                supplier.Gender = reader["gender"].ToString();
+                supplier.Address = reader["address"].ToString();
+                supplier.Mail = reader["mail"].ToString();
+                supplier.Phone = reader["phone"].ToString();
+                supplier.CompanyName = reader["company_name"].ToString();
+                supplier.CompanyMail = reader["company_mail"].ToString();
+                supplier.CompanyPhone = reader["company_phone"].ToString();
+            }
+            
+            reader.Close();
+            connection.Close();
+            return Ok(supplier);
         }
 
         [HttpPost]
-        public IActionResult Store([FromBody] Supplier newSupplier) // model ile olacak
+        public IActionResult Store([FromBody] CreateSupplierModel newSupplier) // model ile olmali
         {
+            connection.Open();
+            MySqlCommand cmd = new MySqlCommand("insert into Suppliers (name, surname, gender, address, mail, phone, company_name, company_mail, company_phone) values (@p1,@p2,@p3,@p4,@p5,@p6,@p7, @p8, @p9)",connection);
+            cmd.Parameters.AddWithValue("@p1",newSupplier.Name);
+            cmd.Parameters.AddWithValue("@p2",newSupplier.Surname);
+            cmd.Parameters.AddWithValue("@p3",newSupplier.Gender);
+            cmd.Parameters.AddWithValue("@p4",newSupplier.Address);
+            cmd.Parameters.AddWithValue("@p5",newSupplier.Mail);
+            cmd.Parameters.AddWithValue("@p6",newSupplier.Phone);
+            cmd.Parameters.AddWithValue("@p7",newSupplier.CompanyName);
+            cmd.Parameters.AddWithValue("@p8",newSupplier.CompanyMail);
+            cmd.Parameters.AddWithValue("@p9",newSupplier.CompanyPhone);
+            cmd.ExecuteNonQuery();
+            
+            connection.Close();
+            
             return Ok();
         }
 
         [HttpPut("{id}")]
-        public IActionResult Edit(int id, [FromBody] Supplier updatedSupplier) // model ile olacak
+        public IActionResult Edit(int id, [FromBody] Supplier updatedSupplier) // model ile olmali
         {
-           
+            
             return Ok();
         }
 
---
+++ WebApi/Models/UpdateSupplierModel.cs
@@ -0,0 +1,15 @@
+namespace WebApi.Models
+{
+    public class UpdateSupplierModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyMail { get; set; }
+        public string CompanyPhone { get; set; }
+    }
+}
