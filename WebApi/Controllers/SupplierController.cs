1fba18b0212e414c84ba3cf74e6595e8e6e25d10
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-03-05 03:18:54 +0300

    context duzenleniyor, devam edecek

--- WebApi/Controllers/SupplierController.cs
+++ WebApi/Controllers/SupplierController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
 using MySqlConnector;
 using WebApi.DbOperations;
 using WebApi.Models;
@@ -14,112 +15,114 @@ namespace WebApi.Controllers
     [Route("[controller]s")]
     public class SupplierController : ControllerBase
     {
-        // private readonly SupplierDbContext _context;
+        private readonly SupplierDbContext _context;
 
-        // public SupplierController(SupplierDbContext context)
-        // {
-        //     _context = context;
-        // }
+        public SupplierController(SupplierDbContext context)
+        {
+            _context = context;
+        }
 
         // asagida yazdigim db baglantilari duzeltilecek. 
         // DI ile yapmaya calisacagim,  simdilik gecici olarak bu sekilde kullandim.
 
-        MySqlConnection connection = new MySqlConnection("Server=localhost;Database=IceStacks-Supplier;Uid=root;Pwd=55255Ahmet_;");
+        // MySqlConnection connection = new MySqlConnection("Server=localhost;Database=IceStacks-Supplier;Uid=root;Pwd=55255Ahmet_;");
 
         [HttpGet]
         public IActionResult Index()
         {
-            List<GetSuppliersViewModel> suppliers = new List<GetSuppliersViewModel>();
+            // List<GetSuppliersViewModel> suppliers = new List<GetSuppliersViewModel>();
 
-            connection.Open();
+            // connection.Open();
 
-            MySqlCommand cmd = new MySqlCommand("select * from Suppliers", connection);
-            MySqlDataReader reader = cmd.ExecuteReader();
+            // MySqlCommand cmd = new MySqlCommand("select * from Suppliers", connection);
+            // MySqlDataReader reader = cmd.ExecuteReader();
 
-            while(reader.Read())
-            {
-                GetSuppliersViewModel supplier = new GetSuppliersViewModel();
+            // while(reader.Read())
+            // {
+            //     GetSuppliersViewModel supplier = new GetSuppliersViewModel();
 
-                supplier.Name = reader["name"].ToString();
-                supplier.Surname = reader["surname"].ToString();
-                supplier.Gender = reader["gender"].ToString();
-                supplier.Address = reader["address"].ToString();
-                supplier.Mail = reader["mail"].ToString();
-                supplier.Phone = reader["phone"].ToString();
-                supplier.CompanyName = reader["company_name"].ToString();
-                supplier.CompanyMail = reader["company_mail"].ToString();
-                supplier.CompanyPhone = reader["company_phone"].ToString();
+            //     supplier.Name = reader["name"].ToString();
+            //     supplier.Surname = reader["surname"].ToString();
+            //     supplier.Gender = reader["gender"].ToString();
+            //     supplier.Address = reader["address"].ToString();
+            //     supplier.Mail = reader["mail"].ToString();
+            //     supplier.Phone = reader["phone"].ToString();
+            //     supplier.CompanyName = reader["company_name"].ToString();
+            //     supplier.CompanyMail = reader["company_mail"].ToString();
+            //     supplier.CompanyPhone = reader["company_phone"].ToString();
 
-                suppliers.Add(supplier);
-            }
+            //     suppliers.Add(supplier);
+            // }
 
-            reader.Close();
-            connection.Close();
+            // reader.Close();
+            // connection.Close();
+
+            var suppliers = _context.Suppliers.ToList<Supplier>();
             return Ok(suppliers);
         }
 
-        [HttpGet("{id}")]
-        public IActionResult Show(int id) // model ile olmali
-        {
-            connection.Open();
+        // [HttpGet("{id}")]
+        // public IActionResult Show(int id) // model ile olmali
+        // {
+        //     connection.Open();
 
-            MySqlCommand cmd = new MySqlCommand("select * from Suppliers where id = @id", connection);
-            cmd.Parameters.AddWithValue("@id", id);
+        //     MySqlCommand cmd = new MySqlCommand("select * from Suppliers where id = @id", connection);
+        //     cmd.Parameters.AddWithValue("@id", id);
             
-            MySqlDataReader reader = cmd.ExecuteReader();
+        //     MySqlDataReader reader = cmd.ExecuteReader();
 
-            GetSupplierDetailViewModel supplier = new GetSupplierDetailViewModel();
+        //     GetSupplierDetailViewModel supplier = new GetSupplierDetailViewModel();
 
-            while(reader.Read())
-            {
-                supplier.Name = reader["name"].ToString();
-                supplier.Surname = reader["surname"].ToString();
-                supplier.Gender = reader["gender"].ToString();
-                supplier.Address = reader["address"].ToString();
-                supplier.Mail = reader["mail"].ToString();
-                supplier.Phone = reader["phone"].ToString();
-                supplier.CompanyName = reader["company_name"].ToString();
-                supplier.CompanyMail = reader["company_mail"].ToString();
-                supplier.CompanyPhone = reader["company_phone"].ToString();
-            }
+        //     while(reader.Read())
+        //     {
+        //         supplier.Name = reader["name"].ToString();
+        //         supplier.Surname = reader["surname"].ToString();
+        //         supplier.Gender = reader["gender"].ToString();
+        //         supplier.Address = reader["address"].ToString();
+        //         supplier.Mail = reader["mail"].ToString();
+        //         supplier.Phone = reader["phone"].ToString();
+        //         supplier.CompanyName = reader["company_name"].ToString();
+        //         supplier.CompanyMail = reader["company_mail"].ToString();
+        //         supplier.CompanyPhone = reader["company_phone"].ToString();
+        //     }
             
-            reader.Close();
-            connection.Close();
-            return Ok(supplier);
-        }
+        //     reader.Close();
+        //     connection.Close();
+        //     return Ok(supplier);
+        // }
 
-        [HttpPost]
-        public IActionResult Store([FromBody] CreateSupplierModel newSupplier) // model ile olmali
-        {
-            connection.Open();
-            MySqlCommand cmd = new MySqlCommand("insert into Suppliers (name, surname, gender, address, mail, phone, company_name, company_mail, company_phone) values (@p1,@p2,@p3,@p4,@p5,@p6,@p7, @p8, @p9)",connection);
-            cmd.Parameters.AddWithValue("@p1",newSupplier.Name);
-            cmd.Parameters.AddWithValue("@p2",newSupplier.Surname);
-            cmd.Parameters.AddWithValue("@p3",newSupplier.Gender);
-            cmd.Parameters.AddWithValue("@p4",newSupplier.Address);
-            cmd.Parameters.AddWithValue("@p5",newSupplier.Mail);
-            cmd.Parameters.AddWithValue("@p6",newSupplier.Phone);
-            cmd.Parameters.AddWithValue("@p7",newSupplier.CompanyName);
-            cmd.Parameters.AddWithValue("@p8",newSupplier.CompanyMail);
-            cmd.Parameters.AddWithValue("@p9",newSupplier.CompanyPhone);
-            cmd.ExecuteNonQuery();
+        // [HttpPost]
+        // public IActionResult Store([FromBody] CreateSupplierModel newSupplier) // model ile olmali
+        // {
+        //     connection.Open();
+        //     MySqlCommand cmd = new MySqlCommand("insert into Suppliers (name, surname, gender, address, mail, phone, company_name, company_mail, company_phone) values (@p1,@p2,@p3,@p4,@p5,@p6,@p7, @p8, @p9)",connection);
+        //     cmd.Parameters.AddWithValue("@p1",newSupplier.Name);
+        //     cmd.Parameters.AddWithValue("@p2",newSupplier.Surname);
+        //     cmd.Parameters.AddWithValue("@p3",newSupplier.Gender);
+        //     cmd.Parameters.AddWithValue("@p4",newSupplier.Address);
+        //     cmd.Parameters.AddWithValue("@p5",newSupplier.Mail);
+        //     cmd.Parameters.AddWithValue("@p6",newSupplier.Phone);
+        //     cmd.Parameters.AddWithValue("@p7",newSupplier.CompanyName);
+        //     cmd.Parameters.AddWithValue("@p8",newSupplier.CompanyMail);
+        //     cmd.Parameters.AddWithValue("@p9",newSupplier.CompanyPhone);
+        //     cmd.ExecuteNonQuery();
             
-            connection.Close();
+        //     connection.Close();
             
-            return Ok();
-        }
+        //     return Ok();
+        // }
 
-        [HttpPut("{id}")]
-        public IActionResult Edit(int id, [FromBody] Supplier updatedSupplier) // model ile olmali
-        {
+        // [HttpPut("{id}")]
+        // public IActionResult Edit(int id, [FromBody] Supplier updatedSupplier) // model ile olmali
+        // {
             
-            return Ok();
-        }
+        //     return Ok();
+        // }
 
-        [HttpDelete("{id}")]
-        public IActionResult Destroy(int id)
-        {
-            return Ok();
-        }
+        // [HttpDelete("{id}")]
+        // public IActionResult Destroy(int id)
+        // {
+        //     return Ok();
+        // }
     }
 }
--- WebApi/DbOperations/SupplierDbContext.cs
+++ WebApi/DbOperations/SupplierDbContext.cs
@@ -1,20 +1,25 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using MySqlConnector;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
 using WebApi.Models;
 
 namespace WebApi.DbOperations
 {
     public class SupplierDbContext : DbContext
     {
-        protected readonly MySqlConnection _connection;
 
-        public SupplierDbContext(MySqlConnection connection)
-        {
-            _connection = connection;
-        }
+
+        // protected override void OnConfiguring(DbContextOptionsBuilder options)
+        // {
+        //     // connect to mysql with connection string from app settings
+        //     var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+        //     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        // }
 
         public DbSet<Supplier> Suppliers { get; set; }
-            
+        public SupplierDbContext(DbContextOptions<SupplierDbContext> options) : base(options)     {}
+
     }
 }
